dc7fecf05a8f1d6e64fa5902848d8111bb79915d
Yaroslav <6715414@gmail.com>
2023-01-03 23:04:27 +0200

    added swap array

--- js/index.js
+++ js/index.js
@@ -4,10 +4,15 @@ const arrayTest = generateArray(30, 500);
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
+  stroke(0);
+  frameRate(15);
 }
 
 function draw() {
   background(250);
+  clear();
+
+  sortArray(arrayTest);
   displayArray(arrayTest);
 }
 
@@ -19,12 +24,28 @@ function generateArray(n = 20, max = 20, min = 0) {
   return arrayResult;
 }
 
-function displayArray(array) {
+function displayArray(array = []) {
   strokeWeight(5);
   for (let index = 0; index < array.length; index++) {
     let item = array[index];
     let x = (index + 0.3) * bandSize;
 
     line(x, height - item, x, height);
+  }
+}
+
+function sortArray(array = []) {
+  let swapped = false;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+
+        swapped = true;
+        return swapped;
+      }
+    }
   }
 }
